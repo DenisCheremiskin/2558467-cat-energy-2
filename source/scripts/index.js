3f103c3230c8088bc3bdaf255bbeb869a49055c6
DenisCheremiskin <cheremiskindenis@gmail.com>
2025-01-09 21:58:16 +0700

    Добавляет карту и навигацию

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
 /* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-nav');
+const navigationMenuButton = document.querySelector('.navigation__menu');
+
+mainNav.classList.remove('main-nav-nojs');
+
+navigationMenuButton.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav-closed')) {
+    mainNav.classList.remove('main-nav-closed');
+    mainNav.classList.add('main-nav-opened');
+  } else {
+    mainNav.classList.add('main-nav-closed');
+    mainNav.classList.remove('main-nav-opened');
+  }
+});
+
+navigationMenuButton.addEventListener('click', () => {
+  if (navigationMenuButton.classList.contains('navigation__menu-button')){
+    navigationMenuButton.classList.remove('navigation__menu-button');
+    navigationMenuButton.classList.add('navigation__menu-closed');
+  } else {
+    navigationMenuButton.classList.add('navigation__menu-button');
+    navigationMenuButton.classList.remove('navigation__menu-closed');
+  }
+});
